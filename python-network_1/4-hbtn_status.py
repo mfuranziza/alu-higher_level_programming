315a9c163bb0a0d11b5b8bec6c5e7764d696dbf9
mfuranziza <s.mfuranziz@alustudent.com>
2024-03-24 13:23:35 -0700

    Fix 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- content: {}".format(res.text.strip()))
