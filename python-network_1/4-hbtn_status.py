462cbe7b58a410743bbae023e2b941adb0a2b7da
mfuranziza <s.mfuranziz@alustudent.com>
2024-03-24 13:50:16 -0700

    Final 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,25 +2,11 @@
 """
 Python script that fetches https://intranet.hbtn.io/status
 """
-
 import requests
 
 if __name__ == '__main__':
-    # Sending a GET request to the specified URL
-    res = requests.get("https://intranet.hbtn.io/status")
 
-    # Printing the type of response content
+    res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
-
-    # Checking if the status code of the response is 200 (OK)
-    if res.status_code == 200:
-        # If the status code is 200, check if the content is exactly "OK"
-        if res.text.strip() == "OK":
-            print("\t- content: OK")
-        else:
-            # If the content is not "OK", print the actual content received in the response
-            print("\t- content: {}".format(res.text))
-    else:
-        # If the status code is not 200, print the actual content received in the response
-        print("\t- content: {}".format(res.text))
+    print("\t- content: {}".format(res.text))
