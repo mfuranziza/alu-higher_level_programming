a873ef64cece8ef8262ed586c558ceab2da911e2
mfuranziza <s.mfuranziz@alustudent.com>
2024-03-24 13:25:11 -0700

    Fix 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -9,4 +9,7 @@ if __name__ == '__main__':
     res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text.strip()))
+    if res.status_code == 200:
+      print("\t- content: {}".format('OK'))
+    else:
+    print("\t- content: {}".format(res.text))
